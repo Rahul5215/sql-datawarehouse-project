0084781100e84bbe4dc3a657ab763468a468659f
Rahul5215 <127139130+Rahul5215@users.noreply.github.com>
2025-08-28 22:18:16 +0530

    Update 09_performance_analysis.sql

--- scripts/09_performance_analysis.sql
+++ scripts/09_performance_analysis.sql
@@ -2,26 +2,75 @@
 9. Performance Analysis
 =====================================================================================*/
 
--- Customer lifespan
-SELECT customer_key,
-       MIN(order_date) AS first_order,
-       MAX(order_date) AS last_order,
-       (EXTRACT(YEAR FROM AGE(MAX(order_date), MIN(order_date))) * 12
-        + EXTRACT(MONTH FROM AGE(MAX(order_date), MIN(order_date)))) AS lifespan_months
-FROM gold.fact_sales
-GROUP BY customer_key;
-
--- Average monthly sales per customer (added)
-WITH lifespan AS (
-  SELECT customer_key,
-         MIN(order_date) AS first_order,
-         MAX(order_date) AS last_order,
-         (EXTRACT(YEAR FROM AGE(MAX(order_date), MIN(order_date))) * 12
-          + EXTRACT(MONTH FROM AGE(MAX(order_date), MIN(order_date)))) AS lifespan_months,
-         SUM(sales_amount) AS total_sales
-  FROM gold.fact_sales
-  GROUP BY customer_key
+/*Analyze the yearly performance of the products by comparing their sales to 
+  both the average sales performance of the product and the previous year's sales.*/
+WITH yearly_product_sales AS(
+SELECT
+EXTRACT(YEAR FROM s.order_date) AS order_year,
+p.product_name,
+SUM(s.sales_amount) AS current_sales
+FROM gold.fact_sales s
+JOIN gold.dim_products p
+ON s.product_key = p.product_key
+WHERE s.order_date IS NOT NULL
+GROUP BY order_year, p.product_name
 )
-SELECT customer_key, total_sales, lifespan_months,
-       ROUND(total_sales::numeric / NULLIF(lifespan_months,0),2) AS avg_monthly_sales
-FROM lifespan;
+
+SELECT
+order_year,
+product_name,
+current_sales,
+ROUND(AVG(current_sales) OVER(PARTITION BY product_name),2) AS avg_sales,
+current_sales - ROUND(AVG(current_sales) OVER(PARTITION BY product_name),2) AS avg_diff,
+CASE 
+   WHEN current_sales - ROUND(AVG(current_sales) OVER(PARTITION BY product_name),2) > 0 THEN 'Above Average'
+   WHEN current_sales - ROUND(AVG(current_sales) OVER(PARTITION BY product_name),2) < 0 THEN 'Below Average'
+   ELSE 'Average'
+END AS avg_change,
+LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) AS py_sales,
+current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) AS diff_py_sales,
+CASE 
+   WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) > 0 THEN 'Yes'
+   WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) < 0 THEN 'No'
+   ELSE 'Even'
+END AS growth_py
+FROM yearly_product_sales
+ORDER BY product_name,order_year
+
+
+
+/*Analyze the monthly performance of the products by comaparing their sales to 
+  both the average sales performance of the product and the previous year's sales.*/
+WITH monthly_product_sales AS(
+SELECT
+EXTRACT(MONTH FROM s.order_date) AS order_month_num,
+TO_CHAR(s.order_date,'month') AS order_month,
+p.product_name,
+SUM(s.sales_amount) AS current_sales
+FROM gold.fact_sales s
+JOIN gold.dim_products p
+ON s.product_key = p.product_key
+WHERE s.order_date IS NOT NULL
+GROUP BY order_month_num,order_month, p.product_name
+)
+
+SELECT
+order_month,
+product_name,
+current_sales,
+ROUND(AVG(current_sales) OVER(PARTITION BY product_name),2) AS avg_sales,
+current_sales - ROUND(AVG(current_sales) OVER(PARTITION BY product_name),2) AS avg_diff,
+CASE 
+   WHEN current_sales - ROUND(AVG(current_sales) OVER(PARTITION BY product_name),2) > 0 THEN 'Above Average'
+   WHEN current_sales - ROUND(AVG(current_sales) OVER(PARTITION BY product_name),2) < 0 THEN 'Below Average'
+   ELSE 'Average'
+END AS avg_change,
+LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_month_num) AS pm_sales,
+current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_month_num) AS diff_pm_sales,
+CASE 
+   WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_month_num) > 0 THEN 'Yes'
+   WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_month_num) < 0 THEN 'No'
+   ELSE 'Even'
+END AS growth_pm
+FROM monthly_product_sales
+ORDER BY product_name,order_month_num
